ac11bd55e03646c2329abcd9ae4a63960c2d3de3
Rafael Teruya <rafaelteruya@MacBook-Air-de-Rafael.local>
2021-08-15 01:42:43 -0300

    adding pre commit script and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. <code>src/App.tsx</code> and save
+          to reload.
         </p>
         <a
           className="App-link"
