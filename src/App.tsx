55e1a2fa36a71d144e89df5d83c81f6463996ca1
Rafael Teruya <rafaelteruya@MacBook-Air-de-Rafael.local>
2021-08-15 22:38:08 -0300

    adding styled components and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { RecoilRoot } from 'recoil';
 import './App.css';
 import { TestScreen } from './screens/Test/TestScreen';
 
+const theme = createMuiTheme();
 function App() {
   return (
-    <RecoilRoot>
-      <TestScreen />
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <TestScreen />
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Test/TestScreen.tsx
+++ src/screens/Test/TestScreen.tsx
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
 import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
 import { testState } from '../../recoilState/testState';
+
+const TestScreenContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  min-width: 100vw;
+  min-height: 100vh;
+  background: blue;
+`;
 
 export const TestScreen = () => {
   const [counter, setCounter] = useState(0);
   const [test, setTest] = useRecoilState(testState);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-      setTest((prevTest) => [
-        ...prevTest,
-        {
-          id: Date.now().toString(),
-          randomNumber: Math.random(),
-        },
-      ]);
-    }, 1000);
-    return () => clearInterval(interval);
+    // const interval = setInterval(() => {
+    //   setCounter((prevCounter) => prevCounter + 1);
+    //   setTest((prevTest) => [
+    //     ...prevTest,
+    //     {
+    //       id: Date.now().toString(),
+    //       randomNumber: Math.random(),
+    //     },
+    //   ]);
+    // }, 1000);
+    // return () => clearInterval(interval);
   }, []);
   return (
-    <>
+    <TestScreenContainer>
+      <Button variant="contained">Test Material Button</Button>
+
       <div>Counter: {counter}</div>
       <div>
         Test:{' '}
@@ -43,6 +55,6 @@ export const TestScreen = () => {
           </div>
         ))}
       </div>
-    </>
+    </TestScreenContainer>
   );
 };
